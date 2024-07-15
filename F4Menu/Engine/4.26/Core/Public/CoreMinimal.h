4a57317930f00a6a91e3e6d3673559b84a49481b
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2024-07-15 14:55:56 +0200

    remove 4.26

--- F4Menu/Engine/4.26/Core/Public/CoreMinimal.h
+++
@@ -1,9 +0,0 @@
-// CoreMinimal.h
-#pragma once
-
-#include "CoreTypes.h"
-#include "HAL/Platform.h"
-#include "Misc/AssertionMacros.h"
-#include "Math/UnrealMathUtility.h"
-#include "Containers/UnrealString.h"
-#include "Containers/Array.h"
